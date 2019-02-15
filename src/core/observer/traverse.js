0199b74a3a56e852e58a50e4d89277b404c09d2c
wuhao <wuhaosky@163.com>
2019-02-15 13:59:10 +0800

    深度观测

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -22,14 +22,14 @@ function _traverse (val: any, seen: SimpleSet) {
   if ((!isA && !isObject(val)) || Object.isFrozen(val) || val instanceof VNode) {
     return
   }
-  if (val.__ob__) {
+  if (val.__ob__) { // 解决了循环引用导致死循环的问题
     const depId = val.__ob__.dep.id
     if (seen.has(depId)) {
       return
     }
     seen.add(depId)
   }
-  if (isA) {
+  if (isA) { // val[i] 和 val[keys[i]]，这两个参数实际上是在读取子属性的值，这将触发子属性的 get 拦截器函数，保证子属性能够收集到观察者。
     i = val.length
     while (i--) _traverse(val[i], seen)
   } else {
