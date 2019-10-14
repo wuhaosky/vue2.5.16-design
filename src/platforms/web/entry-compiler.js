7d5c29e70f52bdf3a02b4e916f55705d4da2515a
wuhaosky <wuhaosky@163.com>
2019-10-15 01:41:52 +0800

    vue-template-compiler

--- src/platforms/web/entry-compiler.js
+++ src/platforms/web/entry-compiler.js
@@ -1,5 +1,5 @@
 /* @flow */
-
-export { parseComponent } from 'sfc/parser'
-export { compile, compileToFunctions } from './compiler/index'
+// vue-template-compiler 入口文件
+export { parseComponent } from 'sfc/parser'  // 解析单文件组件（Single-File Components）
+export { compile, compileToFunctions } from './compiler/index'  // 编译template为render函数
 export { ssrCompile, ssrCompileToFunctions } from './server/compiler'
