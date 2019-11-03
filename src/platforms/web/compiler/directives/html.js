1efb545f36e5e3bcfa7b0b938ad218d8effc55e3
wuhao <wuhaosky@163.com>
2019-11-03 15:05:47 +0800

    v-html指令的代码生成

--- src/platforms/web/compiler/directives/html.js
+++ src/platforms/web/compiler/directives/html.js
@@ -2,6 +2,18 @@
 
 import { addProp } from 'compiler/helpers'
 
+/**
+  示例：<div v-html="text" ></div>
+  更新el.props属性
+  "props": [
+      {
+          "name": "innerHTML",
+          "value": "_s(text)"
+      }
+  ]
+  生成的render字符串：
+  with(this){return _c('div',{domProps:{"innerHTML":_s(text)}})}
+*/
 export default function html (el: ASTElement, dir: ASTDirective) {
   if (dir.value) {
     addProp(el, 'innerHTML', `_s(${dir.value})`)
