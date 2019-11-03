09740c2a17090082f752134802ff8f954d776396
wuhao <wuhaosky@163.com>
2019-11-03 22:10:55 +0800

    model

--- src/platforms/web/compiler/directives/model.js
+++ src/platforms/web/compiler/directives/model.js
@@ -11,6 +11,14 @@ let warn
 export const RANGE_TOKEN = '__r'
 export const CHECKBOX_RADIO_TOKEN = '__c'
 
+/**
+  用 v-model 指令在表单 <input>、<textarea> 及 <select> 元素上创建双向数据绑定。它会根据控件类型自动选取正确的方法来更新元素。
+
+  v-model 在内部为不同的输入元素使用不同的属性并抛出不同的事件：
+  text 和 textarea 元素使用 value 属性和 input 事件；
+  checkbox 和 radio 使用 checked 属性和 change 事件；
+  select 字段将 value 作为 prop 并将 change 作为事件。
+*/
 export default function model (
   el: ASTElement,
   dir: ASTDirective,
