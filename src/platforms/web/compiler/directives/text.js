60f2de884c57faa5a06fecacaec9c6557dd7412f
wuhao <wuhaosky@163.com>
2019-11-03 17:00:32 +0800

    v-text指令的代码生成

--- src/platforms/web/compiler/directives/text.js
+++ src/platforms/web/compiler/directives/text.js
@@ -2,6 +2,19 @@
 
 import { addProp } from 'compiler/helpers'
 
+/**
+  示例：
+  <div v-text="msg"></div>
+  执行本方法后，更新el.props属性：
+  "props": [
+      {
+          "name": "textContent",
+          "value": "_s(msg)"
+      }
+  ],
+  生成的render字符串：
+  with(this){return _c('div',{domProps:{"textContent":_s(msg)}})}
+ */
 export default function text (el: ASTElement, dir: ASTDirective) {
   if (dir.value) {
     addProp(el, 'textContent', `_s(${dir.value})`)
