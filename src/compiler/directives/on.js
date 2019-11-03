627a2c7a4ac44ce8a00861d7b470098f267a13aa
wuhao <wuhaosky@163.com>
2019-11-03 21:58:08 +0800

    v-on 支持不带参数绑定一个事件/监听器键值对的对象

--- src/compiler/directives/on.js
+++ src/compiler/directives/on.js
@@ -2,6 +2,14 @@
 
 import { warn } from 'core/util/index'
 
+/**
+  v-on 同样支持不带参数绑定一个事件/监听器键值对的对象。注意当使用对象语法时，是不支持任何修饰器的。
+  示例：<button class="btnClass" v-on="{ mousedown: doThis, mouseup: doThat }"></button>
+  增加el.wrapListeners属性：
+  el.wrapListeners = (code) => `_g({staticClass:"btnClass"},{ mousedown: doThis, mouseup: doThat })`
+  生成render字符串：
+  with(this){return _c('button',_g({staticClass:"btnClass"},{ mousedown: doThis, mouseup: doThat }))}
+*/
 export default function on (el: ASTElement, dir: ASTDirective) {
   if (process.env.NODE_ENV !== 'production' && dir.modifiers) {
     warn(`v-on without argument does not support modifiers.`)
