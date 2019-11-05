2a907b173367d65c03f9e31de111c0c99d401ba0
wuhaosky <wuhaosky@163.com>
2019-11-06 01:51:37 +0800

    render-static

--- src/core/instance/render-helpers/render-static.js
+++ src/core/instance/render-helpers/render-static.js
@@ -11,11 +11,11 @@ export function renderStatic (
   let tree = cached[index]
   // if has already-rendered static tree and not inside v-for,
   // we can reuse the same tree.
-  if (tree && !isInFor) {
+  if (tree && !isInFor) { // 如果缓存中已经有vnode，并且当前节点不在for循环中，则直接返回缓存的vnode
     return tree
   }
   // otherwise, render a fresh tree.
-  tree = cached[index] = this.$options.staticRenderFns[index].call(
+  tree = cached[index] = this.$options.staticRenderFns[index].call(  // 只调用一次，下次从cached中取
     this._renderProxy,
     null,
     this // for render fns generated for functional component templates
@@ -53,6 +53,9 @@ function markStatic (
   }
 }
 
+/**
+ * 标记静态vnode的属性
+ */
 function markStaticNode (node, key, isOnce) {
   node.isStatic = true
   node.key = key
