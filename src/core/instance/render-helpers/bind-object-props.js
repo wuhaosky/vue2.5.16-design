0adb7b3ad6aebbba08e5380e9d15a9d299256170
wuhaosky <wuhaosky@163.com>
2019-11-06 21:33:00 +0800

    bindObjectProps

--- src/core/instance/render-helpers/bind-object-props.js
+++ src/core/instance/render-helpers/bind-object-props.js
@@ -11,7 +11,10 @@ import {
 
 /**
  * Runtime helper for merging v-bind="object" into a VNode's data.
- * 把v-bind绑定的属性混入到 VNode's data 属性的on对象中
+ * 1.保留属性（class，style，key,ref,slot,slot-scope,is），如果已存在于VNode's data中，则忽略；否则混入到 VNode's data对象中
+ * 2.使用prop修饰的属性或者mustUseProp的属性，混入到 VNode's data's domProps对象中
+ * 3.使用sync修饰的属性，把update:${key}事件混入到 VNode's data's on对象中
+ * 4.其它属性，混入到 VNode's data's attrs对象中
  */
 export function bindObjectProps (
   data: any,
