e23cc815f006cb125e0af0f9e4e36341f64995e2
wuhao <wuhaosky@163.com>
2019-11-21 10:16:15 +0800

    traverse

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -10,6 +10,7 @@ const seenObjects = new Set()
  * Recursively traverse an object to evoke all converted
  * getters, so that every nested property inside the object
  * is collected as a "deep" dependency.
+ * 递归的遍历对象，收集内嵌属性的依赖（Dep）
  */
 export function traverse (val: any) { // 递归地读取被观察属性的所有子属性的值，这样被观察属性的所有子属性都将会收集到观察者，从而达到深度观测的目的
   _traverse(val, seenObjects)
@@ -23,7 +24,7 @@ function _traverse (val: any, seen: SimpleSet) {
     return
   }
   if (val.__ob__) { // 解决了循环引用导致死循环的问题
-    const depId = val.__ob__.dep.id
+    const depId = val.__ob__.dep.id  // 可观测对象的依赖收集器对象的 id
     if (seen.has(depId)) {
       return
     }
