3270ab27eb692afcc37798cda709c83a2b6cc13f
wuhao <wuhaosky@163.com>
2019-11-27 22:32:56 +0800

    VNodeData

--- flow/vnode.js
+++ flow/vnode.js
@@ -34,31 +34,31 @@ declare type VNodeWithData = {
 
 declare interface VNodeData {
   key?: string | number;
-  slot?: string;
+  slot?: string;                                              // 如果组件是其它组件的子组件，需为插槽指定名称
   ref?: string;
   is?: string;
   pre?: boolean;
   tag?: string;
   staticClass?: string;
-  class?: any;
+  class?: any;                                                // 接受一个字符串、对象或字符串和对象组成的数组
   staticStyle?: { [key: string]: any };
-  style?: Array<Object> | Object;
+  style?: Array<Object> | Object;                             // 接受一个字符串、对象，或对象组成的数组
   normalizedStyle?: Object;
-  props?: { [key: string]: any };
-  attrs?: { [key: string]: string };
-  domProps?: { [key: string]: any };
+  props?: { [key: string]: any };                             // 组件 prop
+  attrs?: { [key: string]: string };                          // 普通的 HTML 特性
+  domProps?: { [key: string]: any };                          // DOM 属性
   hook?: { [key: string]: Function };
-  on?: ?{ [key: string]: Function | Array<Function> };
-  nativeOn?: { [key: string]: Function | Array<Function> };
+  on?: ?{ [key: string]: Function | Array<Function> };        // 事件监听器
+  nativeOn?: { [key: string]: Function | Array<Function> };   // 仅用于组件，用于监听原生事件，而不是组件内部使用。`vm.$emit` 触发的事件。
   transition?: Object;
   show?: boolean; // marker for v-show
   inlineTemplate?: {
     render: Function;
     staticRenderFns: Array<Function>;
   };
-  directives?: Array<VNodeDirective>;
+  directives?: Array<VNodeDirective>;                         // 自定义指令
   keepAlive?: boolean;
-  scopedSlots?: { [key: string]: Function };
+  scopedSlots?: { [key: string]: Function };                  // 作用域插槽的格式为 { name: props => VNode | Array<VNode> }
   model?: {
     value: any;
     callback: Function;
