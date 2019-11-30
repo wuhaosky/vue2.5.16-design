3f3607c188839fbac52845431eac8affab53683e
wuhao <wuhaosky@163.com>
2019-11-30 20:08:14 +0800

    update

---
+++ 笔记/异步更新.js
@@ -0,0 +1,43 @@
+const queue: Array<Watcher> = []
+let has: { [key: number]: ?true } = {}
+let waiting = false
+let flushing = false
+let index = 0
+
+function flushSchedulerQueue () {
+  flushing = true
+  let watcher, id
+
+  queue.sort((a, b) => a.id - b.id)
+
+  for (index = 0; index < queue.length; index++) {
+    watcher = queue[index]
+    id = watcher.id
+    has[id] = null
+    watcher.run()
+  }
+
+  index = queue.length = 0
+  has = {}
+  waiting = flushing = false
+}
+
+export function queueWatcher (watcher: Watcher) {
+  const id = watcher.id
+  if (has[id] == null) {
+    has[id] = true
+    if (!flushing) {
+      queue.push(watcher)
+    } else {
+      let i = queue.length - 1
+      while (i > index && queue[i].id > watcher.id) {
+        i--
+      }
+      queue.splice(i + 1, 0, watcher)
+    }
+    if (!waiting) {
+      waiting = true
+      nextTick(flushSchedulerQueue)
+    }
+  }
+}
