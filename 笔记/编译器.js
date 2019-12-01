3d9afa700d3d766fb560776e387e32eee48ae259
wuhaosky <wuhaosky@163.com>
2019-12-02 01:43:01 +0800

    编译器

---
+++ 笔记/编译器.js
@@ -0,0 +1,15 @@
+function baseCompile (
+  template: string,
+  options: CompilerOptions
+): CompiledResult {
+  const ast = parse(template.trim(), options)
+  if (options.optimize !== false) {
+    optimize(ast, options)
+  }
+  const code = generate(ast, options)
+  return {
+    ast,
+    render: code.render,
+    staticRenderFns: code.staticRenderFns
+  }
+}
